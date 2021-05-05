54154244f975ab74b20b37f3b8aeba5a50333d2f
kthure01 <kent.ho.thureson@gmail.com>
2021-05-05 08:32:50 +0200

    some updates

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='MyFirstWebsite',
+    version='',
+    packages=[
+        'flake8',
+        'pytest',
+        'gspread',
+        'Flask',
+        'gunicorn'
+    ],
+    url='',
+    license='',
+    author='kthure01',
+    author_email='',
+    description=''
+)
